1d5a78452744e99cca138521981ba8b901781d12
Aayush Khangar <khangaralka@gmail.com>
2024-06-11 14:12:28 +0000

    firstCommit

---
+++ webapp/Component.js
@@ -0,0 +1,12 @@
+sap.ui.define(
+    ["sap/suite/ui/generic/template/lib/AppComponent"],
+    function (Component) {
+        "use strict";
+
+        return Component.extend("com.ibspl.worklist.Component", {
+            metadata: {
+                manifest: "json"
+            }
+        });
+    }
+);
